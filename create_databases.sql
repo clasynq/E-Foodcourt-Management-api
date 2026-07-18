ae8efd11b47695dcd85e56b14f7e87c9b73a22e9
Surajit09636 <surajitsutradhar010@gmail.com>
2026-07-18 22:06:08 +0530

    Add create_databases.sql initialization script

---
+++ create_databases.sql
@@ -0,0 +1,7 @@
+-- SQL Script to create the microservice databases for the Smart Food Court Management System
+
+CREATE DATABASE auth_db;
+CREATE DATABASE wallet_db;
+CREATE DATABASE order_db;
+CREATE DATABASE seating_db;
+CREATE DATABASE analytics_db;
