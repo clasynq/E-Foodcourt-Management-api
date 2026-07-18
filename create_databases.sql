7c683ef1c2c5cfa9fa08e6f047fb807ec43597d5
Surajit09636 <surajitsutradhar010@gmail.com>
2026-07-18 22:10:50 +0530

    Update database names in SQL creation script to foodcourt_* prefix

--- create_databases.sql
+++ create_databases.sql
@@ -1,7 +1,7 @@
 -- SQL Script to create the microservice databases for the Smart Food Court Management System
 
-CREATE DATABASE auth_db;
-CREATE DATABASE wallet_db;
-CREATE DATABASE order_db;
-CREATE DATABASE seating_db;
-CREATE DATABASE analytics_db;
+CREATE DATABASE foodcourt_auth;
+CREATE DATABASE foodcourt_wallet;
+CREATE DATABASE foodcourt_order;
+CREATE DATABASE foodcourt_seating;
+CREATE DATABASE foodcourt_analytics;
